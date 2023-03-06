556d8a174cd9d4eb5d104fe0ed8630a6c948ec5e
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-03-06 15:15:28 -0500

    feat(GranthamDistance): add the grantham distance struct

---
+++ src/distance_aa_lib/models/grantham_distance.rs
@@ -0,0 +1,85 @@
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// * Copyright (c) 2022 Cognitive Disorders Research Laboratory
+// *
+// * This project is dual-licensed under the MIT and Apache licenses.
+// *
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// ** APACHE 2.0 LICENSE
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// *     http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// *
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// ** MIT LICENSE
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// *
+// * Permission is hereby granted, free of charge, to any person obtaining a copy
+// * of this software and associated documentation files (the "Software"), to deal
+// * in the Software without restriction, including without limitation the rights
+// * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// * copies of the Software, and to permit persons to whom the Software is
+// * furnished to do so, subject to the following conditions:
+// *
+// * The above copyright notice and this permission notice shall be included in all
+// * copies or substantial portions of the Software.
+// *
+// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// * SOFTWARE.
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+use crate::models::AminoAcid;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct GranthamDistance {
+    first: AminoAcid,
+    second: AminoAcid,
+    distance: usize,
+}
+
+impl GranthamDistance {
+    #[must_use]
+    pub fn new(first: AminoAcid, second: AminoAcid, distance: usize) -> Self {
+        Self {
+            first,
+            second,
+            distance,
+        }
+    }
+    #[must_use]
+    pub fn get_first(&self) -> AminoAcid {
+        self.first.clone()
+    }
+    #[must_use]
+    pub fn get_second(&self) -> AminoAcid {
+        self.second.clone()
+    }
+    #[must_use]
+    pub const fn get_distance(&self) -> usize {
+        self.distance
+    }
+}
+
+impl Default for GranthamDistance {
+    fn default() -> Self {
+        Self {
+            first: AminoAcid::default(),
+            second: AminoAcid::default(),
+            distance: 0,
+        }
+    }
+}
